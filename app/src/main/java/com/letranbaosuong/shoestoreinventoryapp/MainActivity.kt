22e4bcc47b479602ec93b46388aea4ab8919ad83
letranbaosuong <>
2023-05-21 15:56:34 +0700

    update AppBarConfiguration

--- app/src/main/java/com/letranbaosuong/shoestoreinventoryapp/MainActivity.kt
+++ app/src/main/java/com/letranbaosuong/shoestoreinventoryapp/MainActivity.kt
@@ -2,14 +2,27 @@ package com.letranbaosuong.shoestoreinventoryapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.letranbaosuong.shoestoreinventoryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var _activityMainBinding: ActivityMainBinding
+    private lateinit var _appBarConfiguration: AppBarConfiguration
+    private lateinit var _appNavControl: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _appNavControl = findNavController(R.id.app_navigator_fragment)
+        _appBarConfiguration = AppBarConfiguration(_appNavControl.graph)
+        setupActionBarWithNavController(_appNavControl)
+        setContentView(_activityMainBinding.root)
     }
+
+    override fun onNavigateUp(): Boolean =
+        _appNavControl.navigateUp() || super.onSupportNavigateUp()
 }
